b13dd6bb0349406d5eb11c63e2930a013905ccb4
Guilherme Baptista <117361210+Guisbaptista@users.noreply.github.com>
2023-09-26 11:43:49 -0300

    Cadastro

---
+++ cadastro.js
@@ -0,0 +1,12 @@
+let nomePokemons = ["Poochyena", "Zigzagoon", "Dragonite", "Dragonite", "Dragonite", "Poochyena", "Wurmple"]
+let timePokemons = [
+  ["Poochyena", 5, "F", 13],
+  ["Zigzagoon", 3, "F", 8],
+  ["Dragonite", 4, "M", 12],
+  ["Dragonite", 6, "M", 20],
+  ["Dragonite", 6, "F", 15],
+  ["Poochyena", 5, "M", 15],
+  ["Wurmple", 2, "F", 4]
+]
+
+console.log(timePokemons[3][0] + " cadastrado com sucesso")
